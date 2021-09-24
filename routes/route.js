06630372f3b0b13c540ca92c696bad1c9fa2ab38
Olalekan ISMAIL <olalekan.i@menskin.co>
2021-09-24 20:36:47 +0100

    preset

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const search = require('../controllers/search')
 
-router.get('/log', search.search)
+router.get('/', search.search)
 router.post('/log', search.addLog)
 
 module.exports = router;
